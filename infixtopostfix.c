2d5e826b633f6fac10c291fa7292b4cf24b52586
aiktc <aiktc@aiktc87>
2017-08-04 16:16:25 +0530

    parenthesis check

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -1,70 +1,79 @@
 #include<stdio.h>
 #include<ctype.h>
+#define MAX 100
 
-typedef struct conversion
+typedef struct stack
 {
-      char a[30];
-      int top;
+	int a[MAX];
+	int top;
 }stack;
 
-void push(stack*,char);
-char pop(stack*);
-void convert(char[],char[]);
-int priority(char);
+void push(struct stack *ps,char n)
+{
+	if(ps->top!=MAX-1)
+	{
+		ps->top++;
+		ps->a[ps->top]=n;
+	}
+	else
+		printf("\nSTACK IS FULL");
+}
+
+int pop(struct stack *ps)
+{
+	return(ps->a[ps->top--]);
+}
+
+int operation(int n1,int n2,char opr)
+{
+	int ans;
+	switch(opr)
+	{
+		case '+':
+			ans=n1+n2;
+			break;
+		case '*':
+			ans=n1*n2;
+			break;
+		case '-':
+			ans=n1-n2;
+			break;
+		case '/':
+			ans=n1/n2;
+		break;
+	}
+	return(ans);
+}
+
+int evaluate(char post[])
+{
+	stack s1;
+	int i,num1,num2,eval;
+	s1.top=-1;
+	for(i=0;post[i]!='\0';i++)
+	{
+		if(isdigit(post[i])){
+			push(&s1,post[i]-'0');
+			printf("\n%c-%c",post[i]-'0',post[i]);
+		}
+		else
+		{
+			num2=pop(&s1);
+			num1=pop(&s1);
+			eval=operation(num1,num2,post[i]);
+			push(&s1,eval);
+		}
+	}
+	return pop(&s1);
+}
 
 int main()
 {
-   char infix[30],postfix[30];
-   printf("\n Enter INFIX expression");
-   scanf("%s",infix);
-   convert(infix,postfix);
-   printf("\n\nPOSTFIX EXPRESSION \n%s",postfix);
-   return 0;
+	char postfix[30];
+	int result;
+	printf("ENTER THE POSTFIX EXPRESSION\n");
+	scanf("%s",postfix);
+	result=evaluate(postfix);
+	printf("\nANS= %d",result);
+	return 0;
 }
-
-void convert(char in[],char post[])
-{
-      char opr;
-      stack s1;
-      int j=0,i;
-      s1.top=-1;
-      for(i=0;in[i]!='\0';i++)
-         {
-             if(isalpha(in[i]))
-                  post[j++]=in[i];
-             if(in[i]=='(')
-                  push(&s1,in[i]);
-             if(in[i]=='+'||in[i]=='-'||in[i]=='/'||in[i]=='*')
-               {
-                  if(s1.top!=-1)
-                     {    
-                        opr=pop(&s1);
-                        while(priority(opr)>=priority(in[i]))
-                         {
-                             post[j++]=opr;
-                             opr=pop(&s1);
-                         }
-                         push(&s1,opr);
-                         push(&s1,in[i]);
-                     }
-                     else 
-                         push(&s1,in[i]);
-                }
-                 if(in[i]==')')
-                   {
-                       opr=pop(&s1);
-                       while(opr!='(')
-                       {
-                         post[j++]=opr;
-                         opr=pop(&s1);
-                       }
-                    }
-             }
-           while(s1.top!=-1)
-                post[j++]=pop(&s1);
-           post[j]='\0';
-
-
-
-}
-
---
+++ matchpair.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<ctype.h>
+#define MAX 100
+
+typedef struct stack
+{
+	char a[MAX];
+	int top;
+}stack;
+
+void push(struct stack *ps,char n)
+{
+	if(ps->top!=MAX-1)
+	{
+		ps->top++;
+		ps->a[ps->top]=n;
+	}
+	else
+		printf("\nSTACK IS FULL");
+}
+
+char pop(struct stack *ps)
+{
+	return(ps->a[ps->top--]);
+}
+
+int ismatchingpair(char left,char right)
+{
+        switch(left)
+        {
+          case '(':  if(right == ')')
+                     	return 1;
+                     else 
+                        return 0;
+                     break;
+         case '[':  if(right == ']')
+                     	return 1;
+                     else 
+                        return 0;
+                     break;
+         case '{':  if(right == '}')
+                     	return 1;
+                     else 
+                        return 0;
+                     break;
+         default :  return 0;
+        }
+}
+  
+void checkbalanced(char exp[])
+{
+        int i=0;
+        char c;
+        struct stack s;
+        s.top=-1;
+        while(exp[i]!='\0')
+        {
+           if(exp[i] == '('|| exp[i] =='['|| exp[i] =='{')
+             push(&s,exp[i]);  
+           else
+           {
+                if(s.top == -1)
+               { printf("Invalid expression");
+                return;
+           
+               }      
+               c= pop(&s);
+              if(ismatchingpair(c,exp[i]))
+ 	      {
+               i++;
+               continue ;
+              }   
+             else
+             {
+              printf("Invalid expression");
+              return ;
+             }
+          }
+          i++;
+        }
+      if(s.top==-1)
+        printf("VAlid expression");
+        else
+               printf("Invalid expression");
+	
+}
+
+int main()
+{
+	char exp[30];
+	printf("ENTER AN EXPRESSION CONTAINING PARENTHESIS\n");
+	scanf("%s",exp);
+        checkbalanced(exp);
+        return 0;
+}
